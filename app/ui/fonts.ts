3081abc693dc3964f599dffa266b7e4499272554
Renato F Fracaroli <renatoffracaroli@gmail.com>
2024-10-18 15:37:22 -0300

    chore

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Lato, Lusitana } from "next/font/google";
+
+export const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
